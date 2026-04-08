0166f7768cdcfd88b72bcc768e84ec91de6c7a05
秃尾巴的猫 <byron.zhuwenbo@gmail.com>
2026-04-08 21:38:11 +0800

    feat: Update index.ts

--- src/ws/index.ts
+++ src/ws/index.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common'
 import { OtaVersionQueryService } from '@/api/version/ota-version-query.service'
 import { WsService } from './ws.service'
 import { LastMessageService } from '@/api/message/last-message.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UserIdMessage } from '@/entities/user-id.entity'
+import { UniqueIdMessage } from '@/entities/unique-id.entity'
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([UserIdMessage, UniqueIdMessage])],
   providers: [WsService, OtaVersionQueryService, LastMessageService],
   exports: [WsService]
 })
